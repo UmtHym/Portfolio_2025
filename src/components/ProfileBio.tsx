ac69412bbe1a7cfa6126f8b526f5d18bee19b091
UmtHym <120595657+UmtHym@users.noreply.github.com>
2025-02-20 21:25:48 +0100

    Mainly focused on Sidebar,
    - Added a round Bio image at the top,
    - Added a small Bio story but will cut it a little shorter,
    - Fixed/Turned off the Shadcn’s Sheet component (sidebar) auto closing feature,
    - Added Socials icons and their corresponding links to the bottom of the Sidebar

--- src/components/ProfileBio.tsx
+++ src/components/ProfileBio.tsx
@@ -3,11 +3,23 @@ const ProfileBio = () => {
   return (
     <div className="flex flex-col items-center gap-4">
       <div className="h-[250px] w-[250px] overflow-hidden rounded-full">
-        {/* Image */}
+        <img
+          className="rounded-full"
+          src="images/1681908246814.jpeg"
+          alt="Bio image"
+        />
       </div>
-      <h1 className="text-3xl font-semibold">Your Name</h1>
-      <p className="text-gray-600 dark:text-gray-300 text-center">
-        Your bio here
+      <h1 className="text-3xl font-semibold">Umit Hayim</h1>
+      <p className="text-gray-600 dark:text-gray-300 text-wrap">
+        I am a Software Developer based in Barcelona, Spain. I am proficient in
+        both Frontend development and Backend development. My main stack is MERN
+        but Typescript and Nextjs are my go-to if it's a large project. I have a
+        variety of different business experiences including advertising,
+        marketing, and exporting manager. Acknowledging and retaining the
+        creative, consistent, and managerial skills of my previous endeavors, As
+        a software engineer, I aspire to help bring creative projects to life
+        and demonstrate my commitment to developing world-class software
+        solutions.
       </p>
     </div>
   );
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -11,21 +11,37 @@ const SideBar = ({ isOpen, setIsOpen }: SideBarProps) => {
   return (
     <Sheet open={isOpen} onOpenChange={setIsOpen} modal={false}>
       <SheetTrigger className="absolute left-4 top-10 z-10">
+        {/* Menu Button*/}
         <Menu className="h-6 w-6" />
       </SheetTrigger>
       <SheetContent
         side="left"
         className="flex"
-        onPointerDownOutside={(e) => e.preventDefault()}
+        onInteractOutside={(event) => event.preventDefault()}
       >
         <section className="flex h-full w-[450px] flex-col place-items-center gap-4 p-4 lg:shadow-xl relative">
           {/* ProfileBio only shows in desktop */}
           <div className="hidden lg:block">
-            <ProfileBio />
+            <div>
+              <ProfileBio />
+            </div>
           </div>
-          {/* Your sidebar content */}
-          SIDEBAR CONTENT
           {/* Hamburger menu positioned on right edge */}
+          <div>LATEST ACTIVITIES</div>
+          <div className="flex justify-items-center mt-auto gap-5 text-2xl">
+            <a href="https://github.com/UmtHym" className="link">
+              <i className="bi bi-github"></i>
+            </a>
+            <a href="https://x.com/umthym" className="link">
+              <i className="bi bi-twitter-x"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/umit-hayim/" className="link">
+              <i className="bi bi-linkedin"></i>
+            </a>
+            <a href="mailto:umitbcn@gmail.com" className="link">
+              <i className="bi bi-envelope-at-fill"></i>
+            </a>
+          </div>
           <button
             onClick={() => setIsOpen(!isOpen)}
             className="absolute -right-2 top-3"
